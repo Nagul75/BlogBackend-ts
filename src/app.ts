47b224f139eb68a0eed6b77bb32c3198726374f5
Nagul Kanna T <nagulk697@gmail.com>
2025-11-02 19:00:06 +0530

    set up project

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express"
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("Hello world!");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on 3000 ...");
+})
+
+
