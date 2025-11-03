65ebd433e96689b74d33fe34a4753659ecf7e6e8
Nagul Kanna T <nagulk697@gmail.com>
2025-11-03 13:50:54 +0530

    BLG-6 configure prisma session store

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,33 @@
+import "dotenv/config"
 import express, { Request, Response } from "express";
 import session from "express-session";
 import passport from "passport";
 import indexRouter from "./routes/indexRouter.js";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
 
+import "./auth/passport.js"
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 
 app.use(session({
-  secret: "cats",
+  secret: process.env.SESSION_SECRET as string,
   resave: false,
   saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  },
+  store: new PrismaSessionStore(prisma, {
+    checkPeriod: 10 * 60 * 1000,
+  })
 }));
 app.use(passport.session());
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
-
-app.use("/profile", (req, res, next) => {
-  if (!req.isAuthenticated()) {
-    res.json({ "Message": "Not authorised!" })
-  }
-  res.json(req.user);
-})
 
 app.listen(3000, () => {
   console.log("Server running on 3000 ...");
