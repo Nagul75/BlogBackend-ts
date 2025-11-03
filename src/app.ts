00dc4213f3a8b6f2055300667d16100817a4a741
Nagul Kanna T <nagulk697@gmail.com>
2025-11-03 12:54:45 +0530

    BLG-6 Set up basic server

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
-import express, { Request, Response } from "express"
+import express, { Request, Response } from "express";
+import session from "express-session";
+import passport from "passport";
+import indexRouter from "./routes/indexRouter.js";
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send("Hello world!");
-});
+app.use(session({
+  secret: "cats",
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.session());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+
+app.use("/profile", (req, res, next) => {
+  if (!req.isAuthenticated()) {
+    res.json({ "Message": "Not authorised!" })
+  }
+  res.json(req.user);
+})
 
 app.listen(3000, () => {
   console.log("Server running on 3000 ...");
