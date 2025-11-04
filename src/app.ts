b6a819849183ffa9375767eb1cf8161bd2ad014e
Nagul Kanna T <nagulk697@gmail.com>
2025-11-04 09:08:16 +0530

    BLG-5 integrate post router and cors

--- src/app.ts
+++ src/app.ts
@@ -3,21 +3,27 @@ import express, { Request, Response } from "express";
 import session from "express-session";
 import passport from "passport";
 import indexRouter from "./routes/indexRouter.js";
+import postsRouter from "./routes/postsRouter.js";
 import { PrismaClient } from "./generated/prisma/client.js";
 import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import cors from "cors";
 
 import "./auth/passport.js"
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(cors({
+  credentials: true,
+}));
 
 app.use(session({
   secret: process.env.SESSION_SECRET as string,
   resave: false,
   saveUninitialized: false,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24 * 7
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    secure: false,
   },
   store: new PrismaSessionStore(prisma, {
     checkPeriod: 10 * 60 * 1000,
@@ -28,6 +34,7 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/", indexRouter);
+app.use("/api", postsRouter);
 
 app.listen(3000, () => {
   console.log("Server running on 3000 ...");
