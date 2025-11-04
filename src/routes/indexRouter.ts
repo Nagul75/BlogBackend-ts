e6cc39152be8416d4b46d90aeaccdcb7fc4197cc
Nagul Kanna T <nagulk697@gmail.com>
2025-11-04 09:09:01 +0530

    BLG-6 update login route to respond with json instead of redirect

--- src/routes/indexRouter.ts
+++ src/routes/indexRouter.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
+import { Request, Response, NextFunction } from "express";
 import passport from "../auth/passport.js";
 import * as controller from "../controllers/indexController.js"
 const indexRouter = Router();
 
-indexRouter.post("/login", passport.authenticate("local", {
-  successRedirect: "/profile",
-  failureRedirect: "/",
-})
-);
+indexRouter.post("/login", (req: Request, res: Response, next: NextFunction) => {
+  passport.authenticate("local", (err: Error | null, user: Express.User, info: { message?: string } | undefined) => {
+    if (err) return next(err);
+    if (!user) return res.status(401).json({ error: info?.message || "Invalid credentials" });
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+      return res.status(200).json({ message: "Login successful", user });
+    });
+  })(req, res, next);
+});
 
 indexRouter.post("/logout", controller.logoutPost);
 
